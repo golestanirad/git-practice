9faf6a943dd1683f32ce66ce5f476aab30d5b2b7
Sina Golestanirad <sina.golestanirad@farmersedge.ca>
2021-09-01 22:21:31 -0700

    added state to handle showing cart - not finished yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import { useState } from "recat";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart....</div>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
